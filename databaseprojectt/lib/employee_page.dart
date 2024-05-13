73c4fc49fb85c0252a41a85a960f95a4a3d445c3
m7mdtam <mohammedtamimi72@gmail.com>
2024-05-14 02:26:35 +0300

    employee page

---
+++ databaseprojectt/lib/employee_page.dart
@@ -0,0 +1,105 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, avoid_unnecessary_containers
+
+import 'package:databaseprojectt/home_page.dart';
+import 'package:flutter/material.dart';
+// ignore: unnecessary_import
+import 'package:flutter/widgets.dart';
+
+class Employee extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(children: [
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/images/bk.jpeg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      Container(
+        color: Colors.black.withOpacity(0.6),
+      ),
+      Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 250,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              // ignore: sized_box_for_whitespace
+              child: Container(
+                  height: 100,
+                  width: 100,
+                  child: Image(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                        "lib/images/logo22.jpeg",
+                      ))),
+            ),
+            SizedBox(height: 0.5),
+            Text(
+              'CYPRUS AIRLINES',
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+            SizedBox(height: 18.0),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.email),
+                hintText: 'Enter your email',
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 15.0),
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.lock),
+                hintText: 'Enter your password',
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 0, 0, 0)),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => HomePage()));
+                  },
+                  child: Text('Back home'),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 0, 0, 0)),
+                  ),
+                  onPressed: () {},
+                  child: Text('Login'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ]));
+  }
+}
--- databaseprojectt/lib/login_page.dart
+++ databaseprojectt/lib/login_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, avoid_unnecessary_containers
 
+import 'package:databaseprojectt/employee_page.dart';
 import 'package:databaseprojectt/home_page.dart';
 import 'package:databaseprojectt/sign_up.dart';
 import 'package:flutter/material.dart';
@@ -10,127 +11,159 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(children: [
-      Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('lib/images/bk.jpeg'),
-            fit: BoxFit.cover,
+        body: SafeArea(
+      child: Stack(children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('lib/images/bk.jpeg'),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
-      ),
-      Container(
-        color: Colors.black.withOpacity(0.6),
-      ),
-      Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 250,
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(30),
-              // ignore: sized_box_for_whitespace
-              child: Container(
-                  height: 100,
-                  width: 100,
-                  child: Image(
-                      fit: BoxFit.fill,
-                      image: AssetImage(
-                        "lib/images/logo22.jpeg",
-                      ))),
-            ),
-            SizedBox(height: 0.5),
-            Text(
-              'CYPRUS AIRLINES',
-              style: TextStyle(color: Colors.white, fontSize: 30),
-            ),
-            SizedBox(height: 18.0),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.email),
-                hintText: 'Enter your email',
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-            ),
-            SizedBox(height: 15.0),
-            TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.lock),
-                hintText: 'Enter your password',
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ButtonStyle(
-                    foregroundColor: MaterialStateProperty.all<Color>(
-                        const Color.fromARGB(255, 0, 0, 0)),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => HomePage()));
-                  },
-                  child: Text('Back home'),
-                ),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    foregroundColor: MaterialStateProperty.all<Color>(
-                        const Color.fromARGB(255, 0, 0, 0)),
-                  ),
-                  onPressed: () {},
-                  child: Text('Login'),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Not a member ? ',
-                  style: TextStyle(
-                      decoration: TextDecoration.underline,
-                      decorationColor: Colors.white,
-                      fontSize: 17,
-                      color: Colors.white,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(width: 0.0),
-                TextButton(
-                  style: ButtonStyle(
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.blue),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => SignUp()));
-                  },
-                  child: Text(
-                    style: TextStyle(fontSize: 18),
-                    'Sign up',
-                  ),
-                ),
-              ],
-            ),
-          ],
+        Container(
+          color: Colors.black.withOpacity(0.6),
         ),
-      ),
-    ]));
+        Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 250,
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                // ignore: sized_box_for_whitespace
+                child: Container(
+                    height: 100,
+                    width: 100,
+                    child: Image(
+                        fit: BoxFit.fill,
+                        image: AssetImage(
+                          "lib/images/logo22.jpeg",
+                        ))),
+              ),
+              SizedBox(height: 0.5),
+              Text(
+                'CYPRUS AIRLINES',
+                style: TextStyle(color: Colors.white, fontSize: 30),
+              ),
+              SizedBox(height: 18.0),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  hintText: 'Enter your email',
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15.0),
+              TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.lock),
+                  hintText: 'Enter your password',
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          const Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => HomePage()));
+                    },
+                    child: Text('Back home'),
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          const Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                    onPressed: () {},
+                    child: Text('Login'),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a member ? ',
+                    style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.white,
+                        fontSize: 17,
+                        color: Colors.white,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 0.0),
+                  TextButton(
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => SignUp()));
+                    },
+                    child: Text(
+                      style: TextStyle(fontSize: 18),
+                      'Sign up',
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Are you a manager ? ',
+                    style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.white,
+                        fontSize: 17,
+                        color: Colors.white,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 0.0),
+                  TextButton(
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => Employee()));
+                    },
+                    child: Text(
+                      style: TextStyle(fontSize: 18),
+                      'Sign in',
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ]),
+    ));
   }
 }
